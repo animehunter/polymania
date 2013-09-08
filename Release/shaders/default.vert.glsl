a7b05d38b64c6f3c83a177c94893856c0a8639db
hunter <animehunter@gmail.com>
2013-09-08 21:03:09 +1000

    add some weird stretching effect when you move

--- Release/shaders/default.vert.glsl
+++ Release/shaders/default.vert.glsl
@@ -5,8 +5,11 @@ OUT vec4 colorIn;
 uniform mat4 projection;
 uniform mat4 modelview;
 
+uniform float camx;
+uniform float camy;
+
 void main()
 {
    colorIn = color;
-   gl_Position = projection*modelview*vec4(pos, 1.0);
+   gl_Position = projection*modelview*vec4(pos.x*(cos(camx*3.142)+2), pos.y*(sin(camy*3.142)+2), pos.z-2, 1.0);
 }
